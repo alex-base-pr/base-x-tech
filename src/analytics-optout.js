aba6d63c6fcc1e1affa646922af99f5fd5a75c8a
Ihor Kuzchenko <example@gmail.com>
2026-06-30 14:24:17 +0300

    Fix Analytics tracking exceptions, js code moved to head-alt

--- src/analytics-optout.js
+++
@@ -1,4 +0,0 @@
-window.allowTracking =
-  ["base-xtech.com", "www.base-xtech.com", "staging.base-xtech.com"].includes(
-    window.location.hostname,
-  ) && localStorage.getItem("analytics_optout") !== "1";
